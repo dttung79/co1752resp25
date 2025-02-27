994c56de823bdebed9f70f366c9964759a8cf1ce
dttung79 <dttung@gmail.com>
2025-02-27 11:50:02 +0700

    complete w4

---
+++ week3/income_man.py
@@ -0,0 +1,11 @@
+income = 1000
+stuffs = ['food', 'rent', 'entertainment', 'clothes', 'utilities']
+costs = [200, 300, 100, 150, 50]
+
+# Write a program that mange the income and expenses of a person. The program should have the following functions:
+# 1. Print expenses: print the list of expenses and their costs
+# 2. Print the most expensive item
+# 3. Calculate total expenses
+# 4. Get the cost of an item
+# 5. Add an item (stuff and cost by asking the user), if the total cost exceeds the income, don't add item and print a message
+# 6. Exit
